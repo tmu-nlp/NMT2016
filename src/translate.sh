278d26c421d3f5a8bbcf85004b13a8a4e1ae031d
佐藤貴之 <sasatatata99@gmail.com>
2017-02-22 16:06:41 +0900

    bug fix

--- src/translate.sh
+++ src/translate.sh
@@ -6,8 +6,10 @@ DEV_SOURCE_FILE=''
 DEV_TARGET_FILE=''
 TEST_SOURCE_FILE=''
 TEST_TARGET_FILE=''
-
 MODEL_DIR=''
+
+USE_GPU=1
+GPU_DEVICE=0
 VOCAB_SIZE=30000
 EMBED_SIZE=512
 HIDDEN_SIZE=512
@@ -15,28 +17,28 @@ MAXOUT_SIZE=512
 EPOCH_NUM=30
 MINIBATCH=10
 POOLING=100
-GENERATION_LIMIT=60
-#WORD2VEC_SOURCE=''
-#WORD2VEC_TARGET=''
 OPTIMIZER='Adagrad'
 LEARNING_RATE=0.01
-GPU_DEVICE=3
-
+GENERATION_LIMIT=60
 MODEL_NUM='001'
+#WORD2VEC_SOURCE=''
+#WORD2VEC_TARGET=''
 
 if [ $1 = 'train' ]; then  
     mkdir $MODEL_DIR
     python attentionNMT.py --mode train --source $TRAIN_SOURCE_FILE --target $TRAIN_TARGET_FILE \
-    --model $MODEL_DIR/epoch --vocab $VOCAB_SIZE --embed $EMBED_SIZE --hidden $HIDDEN_SIZE \
-    --maxout $MAXOUT_SIZE --epoch $EPOCH_NUM --minibatch $MINIBATCH --pooling $POOLING  \
-    --gpu-device $GPU_DEVICE --optimizer $OPTIMIZER --learning_rate $LEARNING_RATE \
+    --model $MODEL_DIR/epoch --use_gpu $USE_GPU --gpu_device $GPU_DEVICE --vocab $VOCAB_SIZE \
+    --embed $EMBED_SIZE --hidden $HIDDEN_SIZE --maxout $MAXOUT_SIZE --epoch $EPOCH_NUM \
+    --minibatch $MINIBATCH --pooling $POOLING --optimizer $OPTIMIZER --learning_rate $LEARNING_RATE \
     #--word2vec_source $WORD2VEC_SOURCE --word2vec_target $WORD2VEC_TARGET
 elif [ $1 = 'dev' ]; then
     python attentionNMT.py --mode test --source $DEV_SOURCE_FILE --target $DEV_TARGET_FILE \
-    --model $MODEL_DIR/epoch.$MODEL_NUM --minibatch 1 --generation_limit $GENERATION_LIMIT --gpu-device $GPU_DEVICE
+    --model $MODEL_DIR/epoch.$MODEL_NUM --use_gpu $USE_GPU --gpu_device $GPU_DEVICE --minibatch 1 \
+    --generation_limit $GENERATION_LIMIT
 elif [ $1 = 'test' ]; then
     python attentionNMT.py --mode test --source $TEST_SOURCE_FILE --target $TEST_TARGET_FILE \
-    --model $MODEL_DIR/epoch.$MODEL_NUM --minibatch 1 --generation_limit $GENERATION_LIMIT --gpu-device $GPU_DEVICE
+    --model $MODEL_DIR/epoch.$MODEL_NUM --use_gpu $USE_GPU --gpu_device $GPU_DEVICE --minibatch 1 \
+    --generation_limit $GENERATION_LIMIT
 else
     echo 'set the first argument $1 ./translate.sh {} with train, dev, or test' 
 fi
