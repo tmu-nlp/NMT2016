781494819c3f8bcf2d403217d03fb9737d520836
佐藤貴之 <sasatatata99@gmail.com>
2017-03-07 04:08:53 +0900

    rewrite with getopts [update]

--- src/translate.sh
+++ src/translate.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-TRAIN_SOURCE_FILE=''
-TRAIN_TARGET_FILE=''
-DEV_SOURCE_FILE=''
-DEV_TARGET_FILE=''
-TEST_SOURCE_FILE=''
-TEST_TARGET_FILE=''
+MODE=''
+SOURCE_FILE=''
+TARGET_FILE=''
+OUTPUT_FILE=''
 MODEL_DIR=''
-
 USE_GPU=1
 GPU_DEVICE=0
 VOCAB_SIZE=30000
@@ -15,31 +12,50 @@ EMBED_SIZE=512
 HIDDEN_SIZE=512
 MAXOUT_SIZE=512
 EPOCH_NUM=30
-MINIBATCH=10
+BATCH_SIZE=10
 POOLING=100
 OPTIMIZER='Adagrad'
 LEARNING_RATE=0.01
 GENERATION_LIMIT=60
-MODEL_NUM='001'
 #WORD2VEC_SOURCE=''
 #WORD2VEC_TARGET=''
+MODEL_NUM='001'
 
-if [ $1 = 'train' ]; then  
+while getopts a:s:t:o:m:n:u:g:v:e:h:x:c:b:p:z:l:i: OPT
+do
+    case $OPT in
+        a ) MODE=$OPTARG ;; 
+        s ) SOURCE_FILE=$OPTARG ;; 
+        t ) TARGET_FILE=$OPTARG ;; 
+        o ) OUTPUT_FILE=$OPTARG ;;
+        m ) MODEL_DIR=$OPTARG ;;
+        n ) MODEL_NUM=$OPTARG ;;
+        u ) USE_GPU=$OPTARG ;;
+        g ) GPU_DEVICE=$OPTARG ;;
+        v ) VOCAB_SIZE=$OPTARG ;;
+        e ) EMBED_SIZE=$OPTARG ;;
+        h ) HIDDEN_SIZE=$OPTARG ;;
+        x ) MAXOUT_SIZE=$OPTARG ;;
+        c ) EPOCH_NUM=$OPTARG ;;
+        b ) BATCH_SIZE=$OPTARG ;;
+        p ) POOLING=$OPTARG ;;
+        z ) OPTIMIZER=$OPTARG ;;
+        l ) LEARNING_RATE=$OPTARG ;;
+        i ) GENERATION_LIMIT=$OPTARG ;;
+    esac
+done
+
+if [ "$MODE" = "train" ]; then
     mkdir $MODEL_DIR
-    python attentionNMT.py --mode train --source $TRAIN_SOURCE_FILE --target $TRAIN_TARGET_FILE \
-    --model $MODEL_DIR/epoch --use_gpu $USE_GPU --gpu_device $GPU_DEVICE --vocab $VOCAB_SIZE \
-    --embed $EMBED_SIZE --hidden $HIDDEN_SIZE --maxout $MAXOUT_SIZE --epoch $EPOCH_NUM \
-    --minibatch $MINIBATCH --pooling $POOLING --optimizer $OPTIMIZER --learning_rate $LEARNING_RATE \
-    #--word2vec_source $WORD2VEC_SOURCE --word2vec_target $WORD2VEC_TARGET
-elif [ $1 = 'dev' ]; then
-    python attentionNMT.py --mode test --source $DEV_SOURCE_FILE --target $DEV_TARGET_FILE \
-    --model $MODEL_DIR/epoch.$MODEL_NUM --use_gpu $USE_GPU --gpu_device $GPU_DEVICE --minibatch 1 \
-    --generation_limit $GENERATION_LIMIT
-elif [ $1 = 'test' ]; then
-    python attentionNMT.py --mode test --source $TEST_SOURCE_FILE --target $TEST_TARGET_FILE \
-    --model $MODEL_DIR/epoch.$MODEL_NUM --use_gpu $USE_GPU --gpu_device $GPU_DEVICE --minibatch 1 \
-    --generation_limit $GENERATION_LIMIT
-else
-    echo 'set the first argument $1 ./translate.sh {} with train, dev, or test' 
+    MODEL=$MODEL_DIR/epoch
+elif [ "$MODE" = "test" ]; then
+    MINIBATCH=1
+    MODEL=$MODEL_DIR/epoch.$MODEL_NUM
+    TARGET_FILE=$OUTPUT_FILE
 fi
+
+python attentionNMT.py --mode "$MODE" --source "$SOURCE_FILE" --target "$TARGET_FILE" \
+       --model "$MODEL" --use_gpu "$USE_GPU" --gpu_device "$GPU_DEVICE" --vocab "$VOCAB_SIZE" \
+       --embed "$EMBED_SIZE" --hidden "$HIDDEN_SIZE" --maxout "$MAXOUT_SIZE" --epoch "$EPOCH_NUM" \
+       --minibatch "$BATCH_SIZE" --pooling "$POOLING" --optimizer "$OPTIMIZER" --learning_rate "$LEARNING_RATE" \
 
